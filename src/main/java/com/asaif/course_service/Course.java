8632d9dd02f292b9021cbefe2d9d58e9e88712d3
Ahmed Saif <asaif@sumerge.com>
2025-04-14 20:32:14 +0200

    add course entity

---
+++ src/main/java/com/asaif/course_service/Course.java
@@ -0,0 +1,48 @@
+package com.asaif.course_service;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String description;
+
+    public Course() {
+
+    }
+    public Course(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
