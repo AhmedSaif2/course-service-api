0bf94a7ce6fb66cb2ef9ad9118b15163c17c7139
asaif <asaif@sumerge.com>
2025-04-22 10:57:48 +0200

    replace manual getters and setters with lombok

--- src/main/java/com/asaif/course_service/model/Quote.java
+++ src/main/java/com/asaif/course_service/model/Quote.java
@@ -1,30 +1,15 @@
 package com.asaif.course_service.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Quote {
     private String q;
     private String a;
-
-    public Quote(String q, String a) {
-        this.q = q;
-        this.a = a;
-    }
-
-    public String getQ() {
-        return q;
-    }
-
-    public void setQ(String q) {
-        this.q = q;
-    }
-
-    public String getA() {
-        return a;
-    }
-
-    public void setA(String a) {
-        this.a = a;
-    }
 }
